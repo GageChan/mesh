9d80c6e8150992ce1ab413ac3eda66e6ba425e35
gagechan <gagechan@foxmail.com>
2019-08-23 17:29:04 +0800

    网关集成swagger(待完善)

---
+++ mesh-service-a/src/main/java/com/gagechan/meshservicea/config/SwaggerConfig.java
@@ -0,0 +1,62 @@
+package com.gagechan.meshservicea.config;
+
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description swagger 配置
+ * @Date 2019-08-06 14:37
+ * @Created by GageChan
+ */
+
+@Configuration
+@EnableSwagger2
+@EnableSwaggerBootstrapUI
+public class SwaggerConfig {
+
+    @Bean
+    public Docket defaultApi() {
+//        ParameterBuilder tokenPar = new ParameterBuilder();
+//        List<Parameter> pars = new ArrayList<>();
+//        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).hidden(false).build();
+//        pars.add(tokenPar.build());
+//        List<ApiKey> apiKeyList = new ArrayList();
+//        apiKeyList.add(new ApiKey("token", "token", "header"));
+
+        List<Parameter> parameterList = new ArrayList<>();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(parameterList);
+//                .globalOperationParameters(parameterList).securitySchemes(apiKeyList);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("服务a")
+                .description("apis of member")
+                .termsOfServiceUrl("https://www.superzoo.com.cn/")
+                .version("1.0")
+                .build();
+    }
+
+}
--- mesh-service-b/src/main/java/com/gagechan/meshserviceb/controller/IndexController.java
+++ mesh-service-b/src/main/java/com/gagechan/meshserviceb/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.gagechan.meshserviceb.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/service/b")
+@Api(tags = "首页接口")
 public class IndexController {
 
     @Value("${server.port}")
     private String port;
 
     @RequestMapping("/hi")
+    @ApiOperation("hi")
     public String hi(@RequestParam String name) {
         return "Hi " + name + ", I am from port: "  + port;
     }
